2954db0993e675aed848dae99fcf243581717ca6
962720900 <bin.1.pan.ext@nokia.com>
2018-03-13 16:03:13 +0800

    Update some methods

--- src/main/java/com/nokia/jira/functions/SynchronizeProjectRole.java
+++ src/main/java/com/nokia/jira/functions/SynchronizeProjectRole.java
@@ -47,7 +47,7 @@ public class SynchronizeProjectRole {
 	 * @param projectKey
 	 * @return Map 集合
 	 */
-	public static Map<String, String> getRolesByProjectKey(String JiraHost,String projectKey){
+	public static Map<String, String> getRolesByProjectKey(String JiraHost,String userName,String password,String projectKey){
 		
 		Map<String, String> projectRolesList = new HashMap<String,String>();
 		String key = null;
@@ -55,54 +55,25 @@ public class SynchronizeProjectRole {
 		
 		String restUrl = JiraHost+String.format(GetRoleFromProject, projectKey);
 		
-		GetMethod response = HttpMethod.httpGet(CDCADMIN, CDC_PWD, restUrl);
+		GetMethod response = HttpMethod.httpGet(userName, password, restUrl);
 		
 		if (response!=null && response.getStatusLine().getStatusCode()>=200 && response.getStatusLine().getStatusCode()<=300) {
-			
-			InputStream responseStream = null;
 			try {
-				//将响应结果转换成输入流
-				responseStream = response.getResponseBodyAsStream();
-				//读取输入流
-				BufferedReader bReader = new BufferedReader(new InputStreamReader(responseStream));
-				//字符串操作类
-				StringBuffer resBuffer = new StringBuffer();
-				String resTemp = null;
-				while((resTemp = bReader.readLine()) != null){
-					
-					resBuffer.append(resTemp);
-				}			
+				JSONObject jsonObject = new JSONObject(HttpMethod.convertResponseAsString(response));
 				
-				JSONObject jsonObject = new JSONObject(resBuffer.toString());
-				
-				Iterator iterator = jsonObject.keys();
+				Iterator<String> iterator = jsonObject.keys();
 				
 				while (iterator.hasNext()) {
 					key = iterator.next().toString();
 					value = jsonObject.getString(key);
 					projectRolesList.put(key, value);		
 					
-				}
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				System.out.println("获取数据失败！！！");
+				}				
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 				System.out.println("获取的返回值不是JSON格式！！");
 			}finally {
-				//关闭流释放资源
-				if (responseStream != null) {
-					try {
-						responseStream.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				
 				//释放Http请求资源
 				if (response != null) {
 					
@@ -126,29 +97,38 @@ public class SynchronizeProjectRole {
 	 */
 	public static Set<String> getUsersFromProjectRole(String jiraHost,String userName,String password, String projectKey, String roleName) {
 		
-		 Set<String> usersOfTheRole = new HashSet<String>();
-		
+		Set<String> usersOfTheRole = new HashSet<String>();
+		//获取该role在当前系统中的ID
 		String roleId = ProjectRolesUtil.getRoleIdByRoleName(jiraHost, userName, password, roleName);
 		
 		String restURL = jiraHost+"/rest/api/2/project/"+projectKey+"/role/"+roleId;
 		
 		GetMethod response = HttpMethod.httpGet(userName, password, restURL);
 		
-		try {
-			InputStream inputStream = response.getResponseBodyAsStream();
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-			
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			response.releaseConnection();
+		if (response!=null && response.getStatusLine().getStatusCode()>=200 && response.getStatusLine().getStatusCode()<=300) {
+			try {
+				String strResponse = HttpMethod.convertResponseAsString(response);
+				
+				JSONObject jsonObject = new JSONObject(strResponse);
+				
+				if (jsonObject.getString("actors") != "[]") {
+					
+					JSONArray jsonArray = jsonObject.getJSONArray("actors");
+					
+					for (int i = 0; i < jsonArray.length(); i++) {
+						
+						usersOfTheRole.add(jsonArray.getJSONObject(i).getString("name"));
+					}
+				}
+				
+				
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}finally {
+				response.releaseConnection();
+			}
 		}
-		
-		
-		
 		return usersOfTheRole;
 	}
 	
@@ -158,7 +138,7 @@ public class SynchronizeProjectRole {
 	 * @param projectRoelsList 项目中的roles列表
 	 * @return
 	 */
-	public static Map<String, List<Map<String, String>>> getAllUsersFromProjectRoles(Map<String, String> projectRoelsList){
+	public static Map<String, List<Map<String, String>>> getAllUsersFromProjectRoles(String userName,String password,Map<String, String> projectRoelsList){
 		
 		Map<String, List<Map<String, String>>> allUsersFromProjectRoles = new HashMap<>();
 		//存放用户列表
@@ -174,24 +154,13 @@ public class SynchronizeProjectRole {
 			
 			String restURLOfprojectRole = mEntry.getValue();
 			//根据获取的role的restURL发送http请求，获取这个role中所有的users和groups
-			GetMethod response = HttpMethod.httpGet(CDCADMIN, CDC_PWD, restURLOfprojectRole);
+			GetMethod response = HttpMethod.httpGet(userName, password, restURLOfprojectRole);
 			
 			if (response!=null && response.getStatusLine().getStatusCode()>=200 && response.getStatusLine().getStatusCode()<=300) {
 				
-				InputStream resStream = null;
 				try {
-					//将响应的对象转换成输入流
-					resStream = response.getResponseBodyAsStream();
-					//将输入流放入缓冲区
-					BufferedReader resReader = new BufferedReader(new InputStreamReader(resStream));
-					StringBuffer resBuffer = new StringBuffer();
-					
-					String resTemp = "";
-					while ((resTemp=resReader.readLine()) != null) {
-						resBuffer.append(resTemp);						
-					}
-					//将http请求放回的内容转换成JSON对象
-					JSONObject jsonObject = new JSONObject(resBuffer.toString());
+					//将http请求返回的内容转换成JSON对象
+					JSONObject jsonObject = new JSONObject(HttpMethod.convertResponseAsString(response));
 					//判断当前的role中是否为空
 					if (jsonObject.getString("actors")!="[]") {
 						//当前role中不为空的情况下解析role中users 和groups
@@ -207,19 +176,9 @@ public class SynchronizeProjectRole {
 							
 						}						
 					}					
-				} catch (IOException e) {
-					e.printStackTrace();
 				} catch (JSONException e) {
 					e.printStackTrace();
 				}finally {
-					//关闭输入流
-					if (resStream != null) {
-						try {
-							resStream.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
 					//释放请求资源
 					if (response != null) {
 						response.releaseConnection();
@@ -227,8 +186,10 @@ public class SynchronizeProjectRole {
 				}
 			}else{
 				System.out.println("请求发送失败！"+response.getStatusLine().getStatusCode());
-			}			
-			allUsersFromProjectRoles.put(mEntry.getKey(), users);
+			}
+			if (users.size() != 0) {
+				allUsersFromProjectRoles.put(mEntry.getKey(), users);
+			}
 		}		
 		return allUsersFromProjectRoles;
 	}
@@ -240,47 +201,52 @@ public class SynchronizeProjectRole {
 	 * @param destinationJITAHOST 目标环境
 	 * @param destinationProjectKey 目标项目
 	 */
-	public static void addUsersToProjectRoles(String sourceJIRAHOST,String sourceProjectKey,String destinationJITAHOST,String destinationProjectKey){
+	public static void addUsersToProjectRoles(String sourceJIRAHOST,String sourceUserName,String sourcePassword,String sourceProjectKey,
+			String destinationJITAHOST,String destinationUserName,String destinationPassword,String destinationProjectKey){
 		
 		//获取目标项目中的所有的project roles及其rest URL列表
-		Map<String, String> DestinationProjectRoles = getRolesByProjectKey(destinationJITAHOST,destinationProjectKey);
+		Map<String, String> DestinationProjectRoles = getRolesByProjectKey(destinationJITAHOST,sourceUserName,sourcePassword,destinationProjectKey);
 		
-		Map<String, List<Map<String, String>>> users = getAllUsersFromProjectRoles(getRolesByProjectKey(sourceJIRAHOST, sourceProjectKey));
+		//获取源项目中所有的role及其中的user list
+		Map<String, List<Map<String, String>>> users = getAllUsersFromProjectRoles(sourceUserName,sourcePassword,getRolesByProjectKey(sourceJIRAHOST,sourceUserName,sourcePassword, sourceProjectKey));
 		
 		//遍历参数中传入的project role的用户列表
 		Set<Entry<String, List<Map<String, String>>>> usersSet = users.entrySet();
 		Iterator<Entry<String, List<Map<String, String>>>> usersIterator = usersSet.iterator();
-		String role = null;
+		String roleName = null;
 		List<Map<String, String>> usersFromRole = null;
 		String destinationRestUrl = null;
 		while (usersIterator.hasNext()) {
 			
 			Entry<String, List<Map<String, String>>> mEntry = usersIterator.next();
-			
+			//获取需要遍历的user list集合
 			usersFromRole = mEntry.getValue();
-			//判断当前role中是否有用户
-			if (usersFromRole.size() != 0) {
-				role = mEntry.getKey();
+			System.out.println(usersFromRole);
+			//获取role Name
+			roleName = mEntry.getKey();
+			
+			destinationRestUrl = DestinationProjectRoles.get(roleName);
+			
+			System.out.println(roleName);
+			
+			//获取目标项目中该role的user list
+			Set<String> destinationRoleusers = getUsersFromProjectRole(destinationJITAHOST, destinationUserName, destinationPassword, destinationProjectKey, roleName);
+			System.out.println(destinationRoleusers);
+			
+			//如果存在用户遍历加入
+			String type = null;
+			String usernameOrGroupname = null;
+			String postJSONString = null;
+			for (int i = 0; i < usersFromRole.size(); i++) {
+				Map<String, String> userOrGroup = usersFromRole.get(i);
 				
-				System.out.println(role);
-				destinationRestUrl = DestinationProjectRoles.get(role);
-				//如果存在用户遍历加入
-				String type = null;
-				String usernameOrGroupname = null;
-				String postJSONString = null;
-				for (int i = 0; i < usersFromRole.size(); i++) {
-					Map<String, String> userOrGroup = usersFromRole.get(i);
-					
-					System.out.println(userOrGroup);
+				System.out.println(userOrGroup);
+				
+				usernameOrGroupname = userOrGroup.get("User Name");
+				
+				if (!destinationRoleusers.contains(usernameOrGroupname)) {
 					//判断存放的是user还是group
-					usernameOrGroupname = userOrGroup.get("User Name");
-					
 					type = userOrGroup.get("Type");
-					
-					
-					System.out.println(type);
-					
-					System.out.println("atlassian-user-role-actor".equals(type));
 					
 					//拼接postJasonString
 					if ("atlassian-user-role-actor".equals(type)) {
@@ -291,8 +257,12 @@ public class SynchronizeProjectRole {
 					System.out.println("postJSONString----->"+postJSONString);
 					System.out.println(destinationRestUrl);
 					
+					
+					
 					if (postJSONString != null && destinationRestUrl != null) {
-						HttpResponse httpResponse = HttpMethod.getPostMethodWithAuthorPostJson(CDCADMIN, CDC_PWD, destinationRestUrl, postJSONString);
+												
+						
+						HttpResponse httpResponse = HttpMethod.getPostMethodWithAuthorPostJson(destinationUserName, destinationPassword, destinationRestUrl, postJSONString);
 						
 						System.out.println(usernameOrGroupname+"加入的状态:  "+httpResponse.getStatusLine().getStatusCode());
 					}
@@ -303,10 +273,12 @@ public class SynchronizeProjectRole {
 	
 	public static void main(String[] args) throws Exception {	 
 			 
-//		addUsersToProjectRoles(JIRA_INT,"SWT", JIRA_INT, "RDTH");
+		addUsersToProjectRoles(JIRA_INT, CDCADMIN, CDC_PWD, "RDTH", JIRA_INT, CDCADMIN, CDC_PWD, "SWT");;
 		
 		//get all users from every role of 7705 project
-		/*Map<String, List<Map<String, String>>> users = getAllUsersFromProjectRoles(getRolesByProjectKey(JIRA_Prod, "IXR"));
+		/*Map<String, List<Map<String, String>>> users = getAllUsersFromProjectRoles(CDCADMIN,CDC_PWD,getRolesByProjectKey(JIRA_INT, CDCADMIN,CDC_PWD, "RDTH"));
+		
+		System.out.println(users);
 		
 		Set<Map.Entry<String, List<Map<String, String>>>> userSet = users.entrySet();
 		
@@ -325,27 +297,9 @@ public class SynchronizeProjectRole {
 			sheetNames.add(sheetName);
 		}
 		
-		
-		System.out.println(sheetNames+" sheetName的大小："+sheetNames.size());
-		
-		for (Iterator sheetNamesIterator = sheetNames.iterator(); sheetNamesIterator.hasNext();) {
-			
-			String sheet = (String) sheetNamesIterator.next();
-			
-			
-			if ( users.get(sheet).size() == 0) {
-				
-				sheetNamesIterator.remove();
-				
-			}
-			
-		}
-		
-		System.out.println(sheetNames+" sheetName的大小："+sheetNames.size());
-		
 		String[] titleRow = {"User Name","Type"};
 		
-		String filDir = "d:\\usersFromEveryRoleOf7250.xls";
+		String filDir = "d:\\usersFromEveryRoleOfRDTH.xls";
 		
 		CreateExcelFile.createExcelXls(filDir, sheetNames, titleRow);
 		
@@ -359,17 +313,9 @@ public class SynchronizeProjectRole {
 //		addUsersToProjectRoles(JIRA_Prod, "SAR", JIRA_Prod, "IXR");
 		
 		
+//		System.out.println(getRolesByProjectKey(JIRA_INT, CDCADMIN, CDC_PWD, "SWT"));
 		
-		List<String> teStrings = new ArrayList<>();
-		teStrings.add("A");
-		
-		teStrings.add("B");
-		
-		teStrings.add("C");
-		
-		System.out.println(teStrings);
-		
-		System.out.println(teStrings.contains("C"));
-		
+//		System.out.println(getUsersFromProjectRole(JIRA_INT, CDCADMIN, CDC_PWD, "RDTH", "Administrators"));
+			
 	}
 }
